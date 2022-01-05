70a946e79138b2cc6ce1b7cfae04e87aedc2de9e
Hana Zouaoui <hana.zouaoui@edu.isetcom.tn>
2022-01-05 10:44:23 +0100

    Aleternate event handler syntax

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ console.log(event.target.value);
         <form className='ui form'>
           <div className='field'>
               <label>Image Search</label>
-            <input type='text' onChange={this.onInputChange}/>
+            <input type='text' onChange={(e) => console.log(e.target.value)}/>
           </div>
         </form>
       </div>
